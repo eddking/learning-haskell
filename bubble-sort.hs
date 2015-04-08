a6209128b8a056fcac660b1002bc0f09b7eca9d6
Edd King <edd@academia.edu>
2015-04-08 11:11:50 -0700

    added caveat to bubble sort implementation

--- bubble-sort.hs
+++ bubble-sort.hs
@@ -6,6 +6,8 @@ sort [] = []
 sort [x] = [x]
 sort xs = sort (init p) ++ [last p]
    where p = pass xs
+-- The implementation of `++` means we're traversing this list way more than
+-- you should have to do for bubble sort. but conceptually, this is correct
 
 pass :: Ord a => [a] -> [a]
 pass [] = []
